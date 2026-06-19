23b61923c99544bc9a6e6dccd650c3c897d1dd1b
guoxf <guoxf@swtsoft.com>
2026-06-19 08:23:38 +0800

    refactor(http): system_config/form_component 改用泛型 AutoHandler

--- http/form_component.go
+++ http/form_component.go
@@ -1,189 +1,85 @@
 package http
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/CloudSilk/pkg/utils/log"
-	"github.com/CloudSilk/pkg/utils/middleware"
-	"github.com/CloudSilk/usercenter/model"
 	apipb "github.com/CloudSilk/usercenter/proto"
+	"github.com/CloudSilk/usercenter/model"
 	"github.com/gin-gonic/gin"
 )
 
 // AddFormComponent godoc
 // @Summary 新增
-// @Description 新增
 // @Tags 表单组件管理
-// @Accept  json
-// @Produce  json
 // @Param authorization header string true "jwt token"
 // @Param data body apipb.FormComponentInfo true "Add FormComponent"
 // @Success 200 {object} apipb.CommonResponse
 // @Router /api/core/auth/form/component/add [post]
-func AddFormComponent(c *gin.Context) {
-	transID := middleware.GetTransID(c)
-	req := &apipb.FormComponentInfo{}
-	resp := &apipb.CommonResponse{
-		Code: apipb.Code_Success,
-	}
-	err := c.BindJSON(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		log.Warnf(context.Background(), "TransID:%s,新建表单组件请求参数无效:%v", transID, err)
-		return
-	}
-	err = middleware.Validate.Struct(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
-
+func AddFormComponent(c *gin.Context, req *apipb.FormComponentInfo) (*apipb.CommonResponse, error) {
 	id, err := model.CreateFormComponent(model.PBToFormComponent(req))
 	if err != nil {
-		resp.Code = apipb.Code_InternalServerError
-		resp.Message = err.Error()
-	} else {
-		resp.Message = id
+		return &apipb.CommonResponse{Code: apipb.Code_InternalServerError, Message: err.Error()}, nil
 	}
-	c.JSON(http.StatusOK, resp)
+	return &apipb.CommonResponse{Code: apipb.Code_Success, Message: id}, nil
 }
 
 // UpdateFormComponent godoc
 // @Summary 更新
-// @Description 更新
 // @Tags 表单组件管理
-// @Accept  json
-// @Produce  json
 // @Param authorization header string true "jwt token"
 // @Param data body apipb.FormComponentInfo true "Update FormComponent"
 // @Success 200 {object} apipb.CommonResponse
 // @Router /api/core/auth/form/component/update [put]
-func UpdateFormComponent(c *gin.Context) {
-	transID := middleware.GetTransID(c)
-	req := &apipb.FormComponentInfo{}
-	resp := &apipb.CommonResponse{
-		Code: apipb.Code_Success,
+func UpdateFormComponent(c *gin.Context, req *apipb.FormComponentInfo) (*apipb.CommonResponse, error) {
+	if err := model.UpdateFormComponent(model.PBToFormComponent(req)); err != nil {
+		return &apipb.CommonResponse{Code: apipb.Code_InternalServerError, Message: err.Error()}, nil
 	}
-	err := c.BindJSON(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		log.Warnf(context.Background(), "TransID:%s,更新表单组件请求参数无效:%v", transID, err)
-		return
-	}
-	err = middleware.Validate.Struct(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
-	err = model.UpdateFormComponent(model.PBToFormComponent(req))
-	if err != nil {
-		resp.Code = apipb.Code_InternalServerError
-		resp.Message = err.Error()
-	}
-	c.JSON(http.StatusOK, resp)
+	return &apipb.CommonResponse{Code: apipb.Code_Success}, nil
 }
 
 // DeleteFormComponent godoc
 // @Summary 删除
-// @Description 删除
 // @Tags 表单组件管理
-// @Accept  json
-// @Produce  json
 // @Param authorization header string true "jwt token"
 // @Param data body apipb.DelRequest true "Delete FormComponent"
 // @Success 200 {object} apipb.CommonResponse
 // @Router /api/core/auth/form/component/delete [delete]
-func DeleteFormComponent(c *gin.Context) {
-	transID := middleware.GetTransID(c)
-	req := &apipb.DelRequest{}
-	resp := &apipb.CommonResponse{
-		Code: apipb.Code_Success,
+func DeleteFormComponent(c *gin.Context, req *apipb.DelRequest) (*apipb.CommonResponse, error) {
+	if err := model.DeleteFormComponent(req.Id); err != nil {
+		return &apipb.CommonResponse{Code: apipb.Code_InternalServerError, Message: err.Error()}, nil
 	}
-	err := c.BindJSON(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		log.Warnf(context.Background(), "TransID:%s,删除表单组件请求参数无效:%v", transID, err)
-		return
-	}
-	err = middleware.Validate.Struct(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
-	err = model.DeleteFormComponent(req.Id)
-	if err != nil {
-		resp.Code = apipb.Code_InternalServerError
-		resp.Message = err.Error()
-	}
-	c.JSON(http.StatusOK, resp)
+	return &apipb.CommonResponse{Code: apipb.Code_Success}, nil
 }
 
 // QueryFormComponent godoc
 // @Summary 分页查询
-// @Description 分页查询
 // @Tags 表单组件管理
-// @Accept  json
-// @Produce  octet-stream
 // @Param authorization header string true "jwt token"
 // @Param pageIndex query int false "从1开始"
 // @Param pageSize query int false "默认每页10条"
-// @Param orderField query string false "排序字段"
-// @Param desc query bool false "是否倒序排序"
-// @Param name query string false "组件名称"
 // @Success 200 {object} apipb.QueryFormComponentResponse
 // @Router /api/core/auth/form/component/query [get]
-func QueryFormComponent(c *gin.Context) {
-	req := &apipb.QueryFormComponentRequest{}
-	resp := &apipb.QueryFormComponentResponse{
-		Code: apipb.Code_Success,
-	}
-	err := c.BindQuery(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
+func QueryFormComponent(c *gin.Context, req *apipb.QueryFormComponentRequest) (*apipb.QueryFormComponentResponse, error) {
+	resp := &apipb.QueryFormComponentResponse{Code: apipb.Code_Success}
 	model.QueryFormComponent(req, resp, false)
-
-	c.JSON(http.StatusOK, resp)
+	return resp, nil
 }
 
 // GetFormComponentDetail godoc
 // @Summary 查询明细
-// @Description 查询明细
 // @Tags 表单组件管理
-// @Accept  json
-// @Produce  json
 // @Param id query string true "ID"
 // @Param authorization header string true "jwt token"
 // @Success 200 {object} apipb.GetFormComponentDetailResponse
 // @Router /api/core/auth/form/component/detail [get]
 func GetFormComponentDetail(c *gin.Context) {
-	resp := &apipb.GetFormComponentDetailResponse{
-		Code: apipb.Code_Success,
-	}
+	resp := &apipb.GetFormComponentDetailResponse{Code: apipb.Code_Success}
 	idStr := c.Query("id")
 	if idStr == "" {
 		resp.Code = apipb.Code_BadRequest
 		c.JSON(http.StatusOK, resp)
 		return
 	}
-	var err error
-
 	data, err := model.GetFormComponentByID(idStr)
 	if err != nil {
 		resp.Code = apipb.Code_InternalServerError
@@ -196,10 +92,9 @@ func GetFormComponentDetail(c *gin.Context) {
 
 func RegisterFormComponentRouter(r *gin.Engine) {
 	g := r.Group("/api/core/auth/form/component")
-
-	g.POST("add", AddFormComponent)
-	g.PUT("update", UpdateFormComponent)
-	g.GET("query", QueryFormComponent)
-	g.DELETE("delete", DeleteFormComponent)
+	g.POST("add", AutoHandler(AddFormComponent))
+	g.PUT("update", AutoHandler(UpdateFormComponent))
+	g.GET("query", AutoQueryHandler(QueryFormComponent))
+	g.DELETE("delete", AutoHandler(DeleteFormComponent))
 	g.GET("detail", GetFormComponentDetail)
 }
--- http/system_config.go
+++ http/system_config.go
@@ -1,188 +1,85 @@
 package http
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/CloudSilk/pkg/utils/log"
-	"github.com/CloudSilk/pkg/utils/middleware"
-	"github.com/CloudSilk/usercenter/model"
 	apipb "github.com/CloudSilk/usercenter/proto"
+	"github.com/CloudSilk/usercenter/model"
 	"github.com/gin-gonic/gin"
 )
 
 // AddSystemConfig godoc
 // @Summary 新增
-// @Description 新增
 // @Tags 系统配置管理
-// @Accept  json
-// @Produce  json
 // @Param authorization header string true "jwt token"
 // @Param account body apipb.SystemConfigInfo true "Add SystemConfig"
 // @Success 200 {object} apipb.CommonResponse
 // @Router /api/core/system/config/add [post]
-func AddSystemConfig(c *gin.Context) {
-	transID := middleware.GetTransID(c)
-	req := &apipb.SystemConfigInfo{}
-	resp := &apipb.CommonResponse{
-		Code: apipb.Code_Success,
-	}
-	err := c.BindJSON(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		log.Warnf(context.Background(), "TransID:%s,新建系统配置请求参数无效:%v", transID, err)
-		return
-	}
-	err = middleware.Validate.Struct(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
-
+func AddSystemConfig(c *gin.Context, req *apipb.SystemConfigInfo) (*apipb.CommonResponse, error) {
 	id, err := model.CreateSystemConfig(model.PBToSystemConfig(req))
 	if err != nil {
-		resp.Code = apipb.Code_InternalServerError
-		resp.Message = err.Error()
-	} else {
-		resp.Message = id
+		return &apipb.CommonResponse{Code: apipb.Code_InternalServerError, Message: err.Error()}, nil
 	}
-	c.JSON(http.StatusOK, resp)
+	return &apipb.CommonResponse{Code: apipb.Code_Success, Message: id}, nil
 }
 
 // UpdateSystemConfig godoc
 // @Summary 更新
-// @Description 更新
 // @Tags 系统配置管理
-// @Accept  json
-// @Produce  json
 // @Param authorization header string true "jwt token"
 // @Param account body apipb.SystemConfigInfo true "Update SystemConfig"
 // @Success 200 {object} apipb.CommonResponse
 // @Router /api/core/system/config/update [put]
-func UpdateSystemConfig(c *gin.Context) {
-	transID := middleware.GetTransID(c)
-	req := &apipb.SystemConfigInfo{}
-	resp := &apipb.CommonResponse{
-		Code: apipb.Code_Success,
+func UpdateSystemConfig(c *gin.Context, req *apipb.SystemConfigInfo) (*apipb.CommonResponse, error) {
+	if err := model.UpdateSystemConfig(model.PBToSystemConfig(req)); err != nil {
+		return &apipb.CommonResponse{Code: apipb.Code_InternalServerError, Message: err.Error()}, nil
 	}
-	err := c.BindJSON(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		log.Warnf(context.Background(), "TransID:%s,更新系统配置请求参数无效:%v", transID, err)
-		return
-	}
-	err = middleware.Validate.Struct(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
-	err = model.UpdateSystemConfig(model.PBToSystemConfig(req))
-	if err != nil {
-		resp.Code = apipb.Code_InternalServerError
-		resp.Message = err.Error()
-	}
-	c.JSON(http.StatusOK, resp)
+	return &apipb.CommonResponse{Code: apipb.Code_Success}, nil
 }
 
 // DeleteSystemConfig godoc
 // @Summary 删除
-// @Description 删除
 // @Tags 系统配置管理
-// @Accept  json
-// @Produce  json
 // @Param authorization header string true "jwt token"
 // @Param data body apipb.DelRequest true "Delete SystemConfig"
 // @Success 200 {object} apipb.CommonResponse
 // @Router /api/core/system/config/delete [delete]
-func DeleteSystemConfig(c *gin.Context) {
-	transID := middleware.GetTransID(c)
-	req := &apipb.DelRequest{}
-	resp := &apipb.CommonResponse{
-		Code: apipb.Code_Success,
+func DeleteSystemConfig(c *gin.Context, req *apipb.DelRequest) (*apipb.CommonResponse, error) {
+	if err := model.DeleteSystemConfig(req.Id); err != nil {
+		return &apipb.CommonResponse{Code: apipb.Code_InternalServerError, Message: err.Error()}, nil
 	}
-	err := c.BindJSON(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		log.Warnf(context.Background(), "TransID:%s,删除系统配置请求参数无效:%v", transID, err)
-		return
-	}
-	err = middleware.Validate.Struct(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
-	err = model.DeleteSystemConfig(req.Id)
-	if err != nil {
-		resp.Code = apipb.Code_InternalServerError
-		resp.Message = err.Error()
-	}
-	c.JSON(http.StatusOK, resp)
+	return &apipb.CommonResponse{Code: apipb.Code_Success}, nil
 }
 
 // QuerySystemConfig godoc
 // @Summary 分页查询
-// @Description 分页查询
 // @Tags 系统配置管理
-// @Accept  json
-// @Produce  octet-stream
 // @Param authorization header string true "jwt token"
 // @Param pageIndex query int false "从1开始"
 // @Param pageSize query int false "默认每页10条"
-// @Param orderField query string false "排序字段"
-// @Param desc query bool false "是否倒序排序"
 // @Success 200 {object} apipb.QuerySystemConfigResponse
 // @Router /api/core/system/config/query [get]
-func QuerySystemConfig(c *gin.Context) {
-	req := &apipb.QuerySystemConfigRequest{}
-	resp := &apipb.QuerySystemConfigResponse{
-		Code: apipb.Code_Success,
-	}
-	err := c.BindQuery(req)
-	if err != nil {
-		resp.Code = apipb.Code_BadRequest
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
+func QuerySystemConfig(c *gin.Context, req *apipb.QuerySystemConfigRequest) (*apipb.QuerySystemConfigResponse, error) {
+	resp := &apipb.QuerySystemConfigResponse{Code: apipb.Code_Success}
 	model.QuerySystemConfig(req, resp, false)
-
-	c.JSON(http.StatusOK, resp)
+	return resp, nil
 }
 
 // GetSystemConfigDetail godoc
 // @Summary 查询明细
-// @Description 查询明细
 // @Tags 系统配置管理
-// @Accept  json
-// @Produce  json
 // @Param id query string true "ID"
 // @Param authorization header string true "jwt token"
 // @Success 200 {object} apipb.GetSystemConfigDetailResponse
 // @Router /api/core/system/config/detail [get]
 func GetSystemConfigDetail(c *gin.Context) {
-	resp := &apipb.GetSystemConfigDetailResponse{
-		Code: apipb.Code_Success,
-	}
+	resp := &apipb.GetSystemConfigDetailResponse{Code: apipb.Code_Success}
 	idStr := c.Query("id")
 	if idStr == "" {
 		resp.Code = apipb.Code_BadRequest
 		c.JSON(http.StatusOK, resp)
 		return
 	}
-	var err error
-
 	data, err := model.GetSystemConfigByID(idStr)
 	if err != nil {
 		resp.Code = apipb.Code_InternalServerError
@@ -193,36 +90,11 @@ func GetSystemConfigDetail(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
-// GetAllSystemConfig godoc
-// @Summary 查询所有
-// @Description 查询所有
-// @Tags 系统配置管理
-// @Accept  json
-// @Produce  json
-// @Param authorization header string true "jwt token"
-// @Success 200 {object} apipb.GetAllSystemConfigResponse
-// @Router /api/core/system/config/all [get]
-func GetAllSystemConfig(c *gin.Context) {
-	resp := &apipb.GetAllSystemConfigResponse{
-		Code: apipb.Code_Success,
-	}
-	list, err := model.GetAllSystemConfigs()
-	if err != nil {
-		resp.Code = apipb.Code_InternalServerError
-		resp.Message = err.Error()
-		c.JSON(http.StatusOK, resp)
-		return
-	}
-	resp.Data = model.SystemConfigsToPB(list)
-	c.JSON(http.StatusOK, resp)
-}
-
 func RegisterSystemConfigRouter(r *gin.Engine) {
 	g := r.Group("/api/core/system/config")
-
-	g.POST("add", AddSystemConfig)
-	g.PUT("update", UpdateSystemConfig)
-	g.GET("query", QuerySystemConfig)
-	g.DELETE("delete", DeleteSystemConfig)
+	g.POST("add", AutoHandler(AddSystemConfig))
+	g.PUT("update", AutoHandler(UpdateSystemConfig))
+	g.GET("query", AutoQueryHandler(QuerySystemConfig))
+	g.DELETE("delete", AutoHandler(DeleteSystemConfig))
 	g.GET("detail", GetSystemConfigDetail)
 }
